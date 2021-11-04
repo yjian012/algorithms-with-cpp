5ce8ace61e5a1c26e33378ead2784bee344bad99
Yi Jiang <yjian012@ucr.edu>
2021-11-03 18:04:45 -0700

    Update geometry.h

--- Computational Geometry/geometry.h
+++ Computational Geometry/geometry.h
@@ -4,6 +4,7 @@
 #include<set>
 #include<vector>
 #include<algorithm>
+//#include<cfloat>
 #include<limits>
 
 using std::vector;
@@ -42,18 +43,17 @@ vector<vector<segment>> read_input(istream &is);
 inline double direction(point pi,point pj,point pk){
     return (pk-pi)*(pj-pi);
 }
-inline bool on_seg(point pi,point pj,point pk){
+inline bool on_seg(const point &pi,const point &pj,const point &pk){
     if(std::min(pi.x,pj.x)<=pk.x&&pk.x<=std::max(pi.x,pj.x)&&std::min(pi.y,pj.y)<=pk.y&&pk.y<=std::max(pi.y,pj.y)) return true;
     return false;
 }
-bool p_sort (point a,point b);
-bool seg_sort (segment a,segment b);
+bool p_sort (const point &a,const point &b);
+bool seg_sort (const segment &a,const segment &b);
 
 void display(const vector<segment> &s);
 void display(const segment &s);
 
-
-auto cmp=[](segment a,segment b){
+auto cmp=[](const segment &a,const segment &b){
     if(a.p1.x<b.p1.x) return direction(a.p1,b.p1,a.p2)>0;
     return direction(b.p1,a.p1,b.p2)<0;
 };
